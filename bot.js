2878a70d1f4ec79ef3d8d467d347d300c0226762
hieudn1995 <53845464+hieudn1995@users.noreply.github.com>
2019-10-08 20:30:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ async function color () {
     forEachTimeout(colors, (color) => {
         client.guilds.forEach((guild) => {
                 if (!stop.includes(guild.id)) {
-                let role = guild.roles.find('name', 'CPH');
+                let role = guild.roles.find('name', '👑 Admin 👑');
                 if (role && role.editable) 
                     role.setColor(color);
             }  
